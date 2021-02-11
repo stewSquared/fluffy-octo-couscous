a70715f883e75d0b559d84ba5bec009a97c09930
Stewart Stewart <stewinsalot@gmail.com>
2021-02-11 09:00:58 -0500

    Test the optimized solution

--- src/test/scala/challenge/Test.scala
+++ src/test/scala/challenge/Test.scala
@@ -5,26 +5,33 @@ import org.junit.Assert._
 
 class Test1 {
   /* Edge cases and properties to test:
-   * [ ] Order of appearance of pairs in each favorites list
+   * [X] Order of appearance of pairs in each favorites list
    * [ ] Order of appearance of favorites lists
    * [ ] Results are normalized
-   * [ ] Threshold off by one
+   * [X] Threshold off by one
    * [ ] Duplicates in a favorites list
    * [ ] Doubled user list == doubled occurrences
    */
 
-  @Test def testNaive(): Unit = {
-    def sample = Seq(
-      Seq("funk1", "funk2", "rock"),
-      Seq("pop", "funk2", "funk1"),
-      Seq("pop", "rock", "country")
-    )
+  val sample1 = Seq(
+    Seq("funk1", "funk2", "rock"),
+    Seq("pop", "funk2", "funk1"),
+    Seq("pop", "rock", "country")
+  )
 
-    val pairsAppearingAtLeastTwice = naiveSolution(sample, threshold = 2)
+  type Solution = (Seq[Seq[String]], Int) => Seq[(String, String)]
+
+  extension (s: Solution) {
+    def run(userFavorites: Seq[Seq[String]], threshold: Int) =
+      s(userFavorites, threshold)
+  }
+
+  def testBasic(solution: Solution): Unit = {
+    val pairsAppearingAtLeastTwice = solution.run(sample1, threshold = 2)
 
     assertEquals(pairsAppearingAtLeastTwice, Seq(("funk1", "funk2")))
 
-    val allPairs = naiveSolution(sample, threshold = 1)
+    val allPairs = solution.run(sample1, threshold = 1)
 
     assertEquals(allPairs.sorted,
       Seq(
@@ -38,5 +45,13 @@ class Test1 {
         ("pop", "rock")
       )
     )
+  }
+
+  @Test def testBasicNaive: Unit = {
+    testBasic(naiveSolution)
+  }
+
+  @Test def testBasicOptimized: Unit = {
+    testBasic(optimizedSolution)
   }
 }
